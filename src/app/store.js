ec524998e12acc0a613c2392852caccfb803fd0d
Ittachi_Uchiha <rakib1001@gmail.com>
2023-03-29 02:21:35 +0600

    'minor update'

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cakeReducer from "../features/cake/cakeSlice"
-import icecreamReducer from "../features/icecream/icecreamSlice"
-import usersReducer from '../features/user/userSlice'
+import cakeReducer from "../features/cake/cakeSlice";
+import icecreamReducer from "../features/icecream/icecreamSlice";
+import usersReducer from "../features/user/userSlice";
 const store = configureStore({
-    reducer:{
-        cake: cakeReducer,
-        icecream: icecreamReducer,
-        users: usersReducer
-    }
-})
+  reducer: {
+    cake: cakeReducer,
+    icecream: icecreamReducer,
+    users: usersReducer,
+  },
+});
 
-export default store
+export default store;
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -2,39 +2,38 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const initialState = {
-    loading: true,
-    users: [],
-    error: ''
-}
+  loading: true,
+  users: [],
+  error: "",
+};
 
 // Generated pending, fulfilled and rejected action types
-export const fetchUsers = createAsyncThunk('user/fetchUsers', () => {
-    return axios.get('https://jsonplaceholder.typicode.com/users')
-    .then((res) => res.data.map((user) => user))
-})
+export const fetchUsers = createAsyncThunk("user/fetchUsers", () => {
+  return axios
+    .get("https://jsonplaceholder.typicode.com/users")
+    .then((res) => res.data.map((user) => user));
+});
 
 const userSlice = createSlice({
-    name: 'user',
-    initialState,
-    extraReducers: (builder) => {
-        builder.addCase(fetchUsers.pending, (state) => {
-          state.loading = true;
-          state.users = [],
-          state.error = ''
-        }),
-          builder.addCase(fetchUsers.fulfilled, (state, action) => {
-            state.loading = false;
-            state.users = action.payload;
-            state.error = ''
+  name: "user",
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(fetchUsers.pending, (state) => {
+      state.loading = true;
+      (state.users = []), (state.error = "");
+    }),
+      builder.addCase(fetchUsers.fulfilled, (state, action) => {
+        state.loading = false;
+        state.users = action.payload;
+        state.error = "";
+      }),
+      builder.addCase(fetchUsers.rejected, (state, action) => {
+        state.loading = false;
+        state.users = [];
+        state.error = action.error.message;
+      });
+  },
+});
 
-          }),
-          builder.addCase(fetchUsers.rejected, (state, action) => {
-            state.loading = false;
-            state.users = [];
-            state.error = action.error.message;
-          });
-    }
-})
-
-export default userSlice.reducer
+export default userSlice.reducer;
 // export { fetchUsers } = userSlice.actions
